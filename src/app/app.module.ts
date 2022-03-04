2c01e78f41e2a4899df24658f4b5fc7f153c0909
Daniel's kiala <danielskiala@gmail.com>
2022-03-04 09:31:48 +0100

    Add HomePage and SinglePage on AppRouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingleComponent } from './single/single.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
